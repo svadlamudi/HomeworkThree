6ce0355bebb37d1a601ce97034f07ef5a21d1410
svadlamudi <skvadlamudi@live.com>
2013-11-17 16:15:47 -0500

    Finalizing Algorithm and Testing

---
+++ vadlamudi_hw3/src/Book.java
@@ -0,0 +1,30 @@
+// Name: Sai Kiran Vadlamudi		Username: svadlamudi		Section: B01
+// Name: Marilda Bozdo				Username: mbozdo			Section: B06
+
+public class Book implements IObject<Book>{
+
+	// Book Object Fields
+	String title;
+	String author;
+	int isbn;
+	
+	// Book Object COnstructor
+	public Book(String title, String author, int isbn){
+		this.title = title;
+		this.author =author;
+		this.isbn = isbn;
+	}
+	
+	// Returns true if the author, title, and isbn of this book is the same as the given book
+	public boolean same(Book t) {
+		return this.title.equals(t.title)    &&
+				this.author.equals(t.author) &&
+				this.isbn == t.isbn;
+	}
+
+	// Returns true if the isbn of this book is less than the given book
+	public boolean lessThan(Book t) {
+		return this.isbn < t.isbn;
+	}
+
+}
--- vadlamudi_hw3/src/MenuItem.java
+++ vadlamudi_hw3/src/MenuItem.java
@@ -3,19 +3,23 @@
 
 public class MenuItem implements IObject<MenuItem>{
 
+	// MenuItem Object Fields
 	String name;
 	int price;
 	
+	// MenuItem Object Constructor
 	public MenuItem(String name, int price){
 		this.name = name;
 		this.price = price;
 	}
 	
+	// Returns true if the name, and price of this MenuItem is the same as the given MenuItem
 	public boolean same(MenuItem t) {
 		return (this.name.equals(t.name) &&
 				this.price == t.price);
 	}
 
+	// Returns true if the price of this MenuItem is less than the given MenuItem
 	public boolean lessThan(MenuItem t) {
 		return this.price < t.price;
 	}
